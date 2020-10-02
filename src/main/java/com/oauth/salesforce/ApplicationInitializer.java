113fb49d56df2b59898e912df854eff6f5a33ab7
Yulia Yakushenkova <yuliya.yakushenkova@customertimes.com>
2020-10-02 19:06:42 +0300

    Some fixes for callback page

--- src/main/java/com/oauth/salesforce/ApplicationInitializer.java
+++ src/main/java/com/oauth/salesforce/ApplicationInitializer.java
@@ -2,19 +2,9 @@ package com.oauth.salesforce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 
-//@Controller
 @SpringBootApplication
 public class ApplicationInitializer {
-
-
-//    @RequestMapping(value = "/callback", produces = MediaType.TEXT_HTML_VALUE)
-//    public String callbackPage() {
-//        return "callback";
-//    }
-
     public static void main(String[] args) {
         SpringApplication.run(ApplicationInitializer.class, args);
     }
